1d06e0eef8fbbe82269d7c0e3e05464a77ac04d3
shenjia <zshenjia@gmail.com>
2019-05-15 05:35:32 +0800

    nothing

--- init.sh
+++ init.sh
@@ -5,14 +5,19 @@ sudo apt-get update
 sudo apt-get install vim -y
 
 # aoeu.sh
-wget https://raw.github.com/shenjia/aoeu.sh/master/linux/aoeu.sh -o ~/aoeu.sh
+wget https://raw.github.com/shenjia/aoeu.sh/master/linux/aoeu.sh -O ~/aoeu.sh
 echo ". ~/aoeu.sh" >> ~/.bash_aliases
 . ~/aoeu.sh
 
 # 8188eu drivers
-sudo wget https://raw.github.com/shenjia/pi/master/drivers/8188eu.ko -o ~/8188eu.ko
+sudo wget https://raw.github.com/shenjia/pi/master/drivers/8188eu.ko -O ~/8188eu.ko
 sudo cp ~/8188eu.ko /lib/modules/*/kernel/net/wireless
-sudo echo "8188eu" >> /etc/modules
+sudo sh -c 'echo "8188eu" >> /etc/modules'
 sudo depmod -a
 sudo modprobe 8188eu
 
+# ip mailer
+sudo wget https://raw.github.com/shenjia/pi/master/ip-mailer.py -O ~/mail_ip.py
+sudo wget https://raw.github.com/shenjia/pi/master/ip-mailer -O /etc/init.d/ip_mailer
+sudo chmod +x /etc/init.d/ip-mailer
+sudo update-rc.d ip-mailer defaults
